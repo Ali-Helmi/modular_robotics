1525033fc2870b64ad237e521967b0027524e0f6
Andrew Hunt <andrewhhunt@tamu.edu>
2023-02-12 16:01:11 -0600

    Created test scripts for testing on PC. Updated controller, robot, and comms scripts

--- Code/RPi/controller.py
+++ Code/RPi/controller.py
@@ -6,6 +6,11 @@ import platform
 import random
 from time import sleep
 
+#import RPi.GPIO as GPIO
+
+import robot
+#import robot_comms
+
 # ANSI commands for moving the cursor arounnd
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 LINE_UP = '\033[1A'     # Moves the cursor up one line
@@ -17,6 +22,11 @@ UNDERLINE = '\033[4m'
 ITALIC = '\033[3m'
 BOLD = '\033[1m'
 
+# Command responses
+BAD_DIRECTION = -1
+BAD_SPEED = -2
+BAD_RESPONSE = -3
+
 def update_objects():
     pass
 
@@ -24,32 +34,40 @@ def update_state():
     pass
 
 
-def camera(data_queue):
-    # runs the computer vision code and sends object and location
-    # data to the robot
-    
-    object = ['ball', 'tape', 'wall']
+def fake_cam_data(data_queue):
+    object_list = ['ball', 'tape', 'wall']
     # Send random data to the robot module for testing
     while True:
         sleep(random.random() * 2)
-        data = [object[random.randint(0,2)], random.randint(1,15)]
+        data = []
+        for x in range(4):
+            data.append([object_list[random.randint(0,2)], random.randint(1,15)])
         data_queue.put(['cam', data])
 
-def robot(data_queue, log_queue):
+def camera(data_queue):
+    # runs the computer vision code and sends object and location
+    # data to the robot
+    fake_cam_data(data_queue)
+    
+    # Real cam data will go here when I get the updated object detection code
+    
+
+def robot_controller(data_queue, comms_queue):
     # Collects object and navigation data from camera to move the 
     # robot around and pick up objects
+    rb = robot.robot(comms_queue)
     
     # clear the terminal screen to show formatted data
     if platform.system() == 'Windows':
         os.system('cls')
     elif platform.system() == 'Linux':
         os.system('clear')
-        
-    last_cam = ''
-    last_com = ''
     
     # set up data screen
-    print('Camera: ' + last_cam + '\nRobot: ' + last_com + '\nCommand: ', end='')
+    print('Detected objects:\n|' + UNDERLINE + '  TYPE  | DISTANCE ' + NORMAL + '|')
+    for x in range(6):
+        print('|        |          |')
+    print('-----------------------------\nCurrent robot state:\nStopped\n-----------------------------\nCommand: ', end='')
     
     while True:
         # Collect data from the queue
@@ -57,56 +75,130 @@ def robot(data_queue, log_queue):
         
         if data[0] == 'cam':
             # reads in data from the camera module. This maintains anything written in the input to not interrupt commands
-            last_cam = str(data[1])
-            print(SAVE_SPOT + LINE_UP + LINE_UP + LINE_UP + LINE_CLEAR + '\rCamera: ' + last_cam  + LOAD_SPOT, end='')
+            print_objects(data[1])
         elif data[0] == 'com':
             # Displays the last command sent, clears the input line
-            last_com = data[1]
-            print(LINE_UP +  LINE_UP + LINE_CLEAR + '\rRobot: ' + last_com + '\n' + LINE_CLEAR + 'Command: ', end='')
-    pass
+            command = data[1].split()
+            command_return = 0
+            direction = command[0].lower()
+            
+            state = ''
+            
+            if direction == 'forward' or direction == 'f':
+                command_return = rb.move_robot(rb.MOTOR_FORWARD, int(command[1]))
+            elif direction == 'back' or direction == 'b':
+                command_return = rb.move_robot(rb.MOTOR_REVERSE, int(command[1]))
+            elif direction == 'right' or direction == 'r':
+                command_return = rb.rotate_robot(rb.MOTOR_FORWARD, int(command[1]))
+            elif direction == 'left' or direction == 'l':
+                command_return = rb.rotate_robot(rb.MOTOR_REVERSE, int(command[1]))
+            elif direction == 'stop' or direction == 's':
+                command_return = rb.stop_robot()
+            else:
+                state = 'error'
+                
+            
+                
+            if rb.state == rb.STOPPED:
+                state = 'Stopped'
+            elif rb.state == rb.MOVING:
+                if rb.direction == rb.FORWARD:
+                    state = 'Moving forward at {}%'.format(rb.speed)
+                elif rb.direction == rb.REVERSE:
+                    state = 'Moving backward at {}%'.format(rb.speed)
+                else:
+                    state = 'ERROR DIRECTION: {}'.format(rb.direction)
+            elif rb.state == rb.ROTATING:
+                if rb.direction == rb.CLOCKWISE:
+                    state = 'Rotating clockwise at {}%'.format(rb.speed)
+                if rb.direction == rb.COUNTERCLOCKWISE:
+                    state = 'Rotating counterclockwise at {}%'.format(rb.speed)
+            else:
+                state = 'ERROR'
+                    
+            if command_return[0] != 1:
+                if command_return[0] == rb.BAD_DIRECTION:
+                    state += ' | Bad direction'
+                elif command_return[0] == rb.BAD_SPEED:
+                    state += ' | Bad speed'
+                else:
+                    non_respond = []
+                    for x in range(1,5):
+                        if command_return[x] != 1:
+                            non_respond.append(x)
+                    
+                    state += ' | non-responding motors: {}'.format(non_respond)
+            
+            print_state(state)
+            print_cmd_recv()
 
-def user_interface(data_queue, log_queue):
-    # takes user input to send commands to the robot
-    sys.stdin = open(0)
-
-    while True:        
-        data = input()
-        if data == 'quit':
-            active = multiprocessing.active_children()
-            for child in active:
-                if child.name != 'ui':
-                    child.close()
-        else:
-            data_queue.put(['com', data])
-
+def print_objects(objects):
+    ui = SAVE_SPOT
+    for x in range(10):
+        ui += LINE_UP
+    ui += '\r'
+    
+    for item in objects:
+        ui += '|  ' + item[0] + '  |   ' + str(item[1]) + '  \n'
+    ui += LOAD_SPOT
+    print(ui, end='')
+    
+def print_state(state):
+    ui = SAVE_SPOT
+    for x in range(3):
+        ui += LINE_UP
+    ui += LINE_CLEAR + '\r' + state + '\n-----------------------------' + LOAD_SPOT
+    print(ui, end='')
+    
+def print_cmd_recv():
+    ui = LINE_UP + LINE_CLEAR + '\rCommand: '
+    print(ui, end='')
+    
+'''
+# Probably not going to be used
+def uart_receive():
+    comms = robot_comms.robot_comms("/dev/ttyS0", 115200, 33)
+    frame = comms.receive_frame()
+    if frame[0] != -1:
+        comms_queue.put(frame)
+'''
 
 if __name__ == '__main__':
+    # Create queues for passing data
     data_queue = multiprocessing.Queue()
-    log_queue = multiprocessing.Queue()
+    comms_queue = multiprocessing.Queue()
     
+    # Create camera and robot processes
     camera_proc = multiprocessing.Process(target=camera, args=(data_queue,), name='camera')
-    robot_proc = multiprocessing.Process(target=robot, args=(data_queue, log_queue,), name='robot')
+    robot_proc = multiprocessing.Process(target=robot_controller, args=(data_queue, comms_queue,), name='robot')
     
-
+    # Start camera and robot processes
     camera_proc.start()
     robot_proc.start()
 
-    #camera_proc.join()
-    #robot_proc.join()
-    #qui_proc.join()
-
+    # reenable input for this process
     sys.stdin = open(0)
 
     while True:        
         data = input()
         if data == 'quit' or data == 'q':
             active = multiprocessing.active_children()
-            # should send terminate command to robot to have it shut off any motors running then terminate
-            # queue.put(terminate command)
+            # Send the stop command to the robot
+            data_queue.put(['com', 's'])
+            # Wait for the robot to deal with the stop command
+            sleep(.5)
+            # Kill all processes and end the program
             camera_proc.terminate()
             robot_proc.terminate()
+            
+            # Clear the terminal
+            #if platform.system() == 'Linux':
+            #    os.system('clear')
+            
+            # Go to a new line so the terminal doesn't end up in a weird spot
+            print('')
             break
         else:
             data_queue.put(['com', data])
 
-    print('CPUs available: ' + str(multiprocessing.cpu_count()))
+    #print('CPUs available: ' + str(multiprocessing.cpu_count()))
--- Code/RPi/robot.py
+++ Code/RPi/robot.py
@@ -1,75 +1,281 @@
+import multiprocessing
 import robot_comms
-
-comms = robot_comms.robot_comms("/dev/ttyS0", 115200)
+from time import sleep
+from datetime import datetime
 
 # Module addresses
-motor_front_left = 0
-motor_front_right = 1
-motor_rear_left = 2
-motor_rear_right = 3
-arm_lift = 4
-arm_gripper = 5
-sensor_left = 6
-sensor_right = 7
-
-#motor directions
-forward = 0
-reverse = 1
+motor_front_left =  1
+motor_front_right = 2
+motor_rear_left =   3
+motor_rear_right =  4
+arm_lift =          5
+arm_gripper =       6
+sensor_left =       7
+sensor_right =      8
 
 
-def move_robot(direction, speed):
-    '''
-        Returns 1 if all the motors in the correct mode, -1 if something went wrong
 
-        Check the direction and speed that is requested
-        if those values are good, send them to the motors
-        Check the returned values from the motors to see if they are in the correct mode
-        If any are wrong, tell all motors to stop
-    '''
-    if direction == 0:
-        left_direction = forward
-        right_direction = reverse
-    elif direction == 1:
-        left_direction = reverse
-        right_direction = forward
-    else:
-        return -1
+class robot:
+    def __init__(self, comms_queue : multiprocessing.Queue) -> None:
+        # Motor directions
+        self.MOTOR_FORWARD = 1
+        self.MOTOR_REVERSE = 2
+        self.MOTOR_COAST = 3
 
-    if speed > 100 or speed < 0:
-        return -1
+        # States
+        self.STOPPED = 1
+        self.MOVING = 2
+        self.ROTATING = 3
+        self.FORWARD = 5
+        self.REVERSE = 6
+        self.CLOCKWISE = 7
+        self.COUNTERCLOCKWISE = 8
+        self.ERROR = 9
+        
+        # Errors
+        self.BAD_DIRECTION =    -1
+        self.BAD_SPEED =        -2
+        self.UART_ERROR =       -4
+        
+        self.comms = robot_comms.robot_comms("/dev/ttyS0", 115200, 33)
+        self.state = self.STOPPED
+        self.direction = self.STOPPED
+        self.speed = 0
+        self.comms_queue = comms_queue
+
+  
+    def motor_frame_to_dict(self, frame):
+        return {
+            'address'   : frame[0],
+            'check'     : frame[1],
+            'direction' : frame[2],
+            'speed'     : frame[3]
+        }
+        
+
+    def send_motor_commands(self, send_data):
+        self.comms.flush_input()
+        
+        self.comms.send_frame(motor_front_left, [send_data['leftdir'], send_data['speed'], send_data['leftcheck']])
+        sleep(0.002)
+        self.comms.send_frame(motor_front_right, [send_data['rightdir'], send_data['speed'], send_data['rightcheck']])
+        sleep(0.002)
+        self.comms.send_frame(motor_rear_left, [send_data['leftdir'], send_data['speed'], send_data['leftcheck']])
+        sleep(0.002)
+        self.comms.send_frame(motor_rear_right, [send_data['rightdir'], send_data['speed'], send_data['rightcheck']])
+        
+        # Grab responses
+        responses = []
+        
+        for x in range(4):
+            responses.append(self.comms.receive_frame(self.comms.MOTOR_FRAME))
+            
+        return responses
+        return 1
+  
     
-    left_check = left_direction ^ speed
-    right_check = right_direction ^ speed
-    comms.send_frame(motor_front_left, [left_direction, speed, left_check])
-    comms.send_frame(motor_front_right, [right_direction, speed, right_check])
-    comms.send_frame(motor_rear_left, [left_direction, speed, left_check])
-    comms.send_frame(motor_rear_right, [right_direction, speed, right_check])
-    # need to add in receiving the frames to check
+    def check_responses(self, sent_data, received_data):
+        
+        response_recieved = [0, 0, 0, 0]
+                
+        for frame in received_data:
+            
+            # If the sent and received ata matches up, set a motor's response
+            # to 1
+            if frame[0] > 0:
+                frame_dict = self.motor_frame_to_dict(frame)
+            
+                if frame_dict['address'] == 1 or frame_dict['address'] == 3:
+                    # Check the left motors
+                    if frame_dict['direction'] == sent_data['leftdir']:
+                        # Check the direction
+                        if frame_dict['speed'] == sent_data['speed']:
+                            # Check the check value
+                            if frame_dict['check'] == sent_data['leftcheck']:
+                                response_recieved[frame_dict['address'] - 1] = 1
+                
+                if frame_dict['address'] == 2 or frame_dict['address'] == 4:
+                    # Check the right motors
+                    if frame_dict['direction'] == sent_data['rightdir']:
+                        # Check the direction
+                        if frame_dict['speed'] == sent_data['speed']:
+                            # Check the check value
+                            if frame_dict['check'] == sent_data['rightcheck']:
+                                response_recieved[frame_dict['address'] - 1] = 1
+                                
+        return response_recieved
+        return 1
 
-    return 1
+    def stop_robot(self):
+        direction = self.MOTOR_COAST
+        speed = 1
+        check = direction ^ speed
+        
+        sent_data = {
+           'leftdir'    : direction,
+           'rightdir'   : direction,
+           'speed'      : speed,
+           'leftcheck'  : check,
+           'rightcheck' : check
+        }
+        
+        responses = self.send_motor_commands(sent_data)
+        
+        checked_responses = self.check_responses(sent_data, responses)
+        
+        comm_error = False
+        
+        for resp in checked_responses:
+            if resp != 1:
+                comm_error = True
+                
+        if comm_error:
+            self.state = self.ERROR
+            self.direction = self.ERROR
+            self.speed = self.ERROR
+            
+            with open('rblog.tx', 'w') as f:
+                for item in sent_data:
+                    f.write(str(sent_data[item]))
+                    f.write('\n')
+                for resp in responses:
+                    f.write(str(resp))
+                    f.write('\n')
+            return [self.UART_ERROR] + checked_responses
+        else:
+            self.state = self.STOPPED
+            self.speed = 0
+            self.direction = self.STOPPED
+            return [1]
 
-def rotate_robot(direction, speed):
-    '''
-        Similar to the move robot function, but in this case spins the motors so that
-        the robot will spin instead of move
-    '''
-    pass
+        
 
-def move_arm(angle):
-    pass
+    def move_robot(self, direction, speed):
+        '''
+            Returns 1 if all the motors in the correct mode
+            -1 if direction value is bad
+            -2 if speed value is bad
+            -3 if not all the motors responded
 
-def toggle_gripper():
-    pass
+            Check the direction and speed that is requested
+            if those values are good, send them to the motors
+            Check the returned values from the motors to see if they are in the correct mode
+            If any are wrong, tell all motors to stop
+        '''
+        
+        if direction == self.MOTOR_FORWARD:
+            left_direction = self.MOTOR_FORWARD
+            right_direction = self.MOTOR_REVERSE
+        elif direction == self.MOTOR_REVERSE:
+            left_direction = self.MOTOR_REVERSE
+            right_direction = self.MOTOR_FORWARD
+        else:
+            return [self.BAD_DIRECTION]
 
-def read_sensor():
-    '''
-        Requests measured distance from the sensor modules and returns
-        a list of the distances
-    '''
+        if speed > 100 or speed < 0:
+            return [self.BAD_SPEED]
+        
+        left_check = left_direction ^ speed
+        right_check = right_direction ^ speed
+        
+        sent_data = {
+           'leftdir'    : left_direction,
+           'rightdir'   : right_direction,
+           'speed'      : speed,
+           'leftcheck'  : left_check,
+           'rightcheck' : right_check
+        }
+        
+        responses = self.send_motor_commands(sent_data)
+        
+        checked_responses = self.check_responses(sent_data, responses)
+        
+        comm_error = False
+        
+        for resp in checked_responses:
+            if resp != 1:
+                comm_error = True
+                
+        if comm_error:
+            self.stop_robot()
+            return [self.UART_ERROR] + checked_responses
+        else:
+            self.state = self.MOVING
+            self.speed = speed
+            if direction == self.MOTOR_FORWARD:
+                self.direction = self.FORWARD
+            elif direction == self.MOTOR_REVERSE:
+                self.direction = self.REVERSE
+            return [1]
+        
+        return 1
     
-    comms.send_frame(sensor_left, [1])
-    distance1 = comms.receive_frame()[0]
-    comms.send_frame(sensor_right, [1])
-    distance2 = comms.receive_frame()[0]
 
-    return [distance1, distance2]
+    def rotate_robot(self, direction, speed):
+        '''
+            Similar to the move robot function, but in this case spins the motors so that
+            the robot will spin instead of move
+        '''
+        
+        if direction == self.MOTOR_FORWARD:
+            rotate_direction = self.MOTOR_FORWARD
+        elif direction == self.MOTOR_REVERSE:
+            rotate_direction = self.MOTOR_REVERSE
+        else:
+            return self.BAD_DIRECTION
+        
+        if speed > 100 or speed < 0:
+            return self.BAD_SPEED
+        
+        check = direction ^ speed
+        
+        sent_data = {
+           'leftdir'    : rotate_direction,
+           'rightdir'   : rotate_direction,
+           'speed'      : speed,
+           'leftcheck'  : check,
+           'rightcheck' : check
+        }
+        
+        responses = self.send_motor_commands(sent_data)
+        
+        checked_responses = self.check_responses(sent_data, responses)
+        
+        comm_error = False
+        
+        for resp in checked_responses:
+            if resp != 1:
+                comm_error = True
+                
+        if comm_error:
+            self.stop_robot()
+            return [self.UART_ERROR] + checked_responses
+        else:
+            self.state = self.ROTATING
+            self.speed = speed
+            if direction == self.MOTOR_FORWARD:
+                self.direction = self.CLOCKWISE
+            elif direction == self.MOTOR_REVERSE:
+                self.direction = self.COUNTERCLOCKWISE
+            return [1]
+        
+        return 1
+
+    def move_arm(self, angle):
+        pass
+
+    def toggle_gripper(self):
+        pass
+
+    def read_sensor(self):
+        '''
+            Requests measured distance from the sensor modules and returns
+            a list of the distances
+        '''
+        
+        self.comms.send_frame(sensor_left, [1])
+        distance1 = self.comms.receive_frame()[0]
+        self.comms.send_frame(sensor_right, [1])
+        distance2 = self.comms.receive_frame()[0]
+
+        return [distance1, distance2]

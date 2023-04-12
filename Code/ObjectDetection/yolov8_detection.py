8a5772ea179116a316989f4e0f5fc58a204fc55f
alihelmi <alihelmi@tamu.edu>
2023-04-12 03:59:17 -0500

    Object Detection & Gripper Arm Updates
    Weekly updates, and updated supporting files for both object detection and gripper arm.

---
+++ Code/ObjectDetection/yolov8_detection.py
@@ -0,0 +1,74 @@
+def object_detection(queue) :
+
+    import cv2
+    import supervision as sv
+    from ultralytics import YOLO
+        
+    cap = cv2.VideoCapture(0)
+    model = YOLO("C:/Users/i3llo/object_detection/best.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+
+    while True:
+        ret, frame = cap.read()
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        frame = box_annotator.annotate(
+            scene=frame, 
+            detections=detections, 
+            labels=labels
+        )
+        
+        detected_objects = []
+        
+        if(len(detections.xyxy) > 0):
+            for i in range(len(detections.xyxy)):
+
+                obj_name = labels[i].split(' ', 1)[0]
+                obj_confidence = float(labels[i].split(' ', 1)[-1])
+
+                x1 = detections.xyxy[i][0]
+                y1 = detections.xyxy[i][1]
+                x2 = detections.xyxy[i][2]
+                y2 = detections.xyxy[i][3]
+
+                center_x = abs(x1 - x2)
+                center_y = abs(y1 - y2)
+
+                obj = {
+                    #IDs (Ball: 0, Bucket: 1, Negative: 2, Tape: 3)
+                    'id'    : detections.class_id[i],
+                    'conf'  : detections.confidence[i],
+                    'x'     : center_x,
+                    'y'     : center_y,
+                }
+
+                if (detections.confidence[i] > 0.6 and detections.class_id[i] != 2) :
+                    detected_objects.append(obj)
+                    print("Object added (name: ", obj_name, " with confidence: ", obj_confidence, ")")
+
+        queue.put(['cam', detected_objects])
+
+
+            
+#         cv2.imshow("yolov8", frame)
+
+#         if (cv2.waitKey(30) == 27): # break with escape key
+#             break
+
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+# if __name__ == "__main__":
+#     object_detection()    
---
+++ Object Dection and Gripper Arm/Object Detection/Source code/README.dataset.txt
@@ -0,0 +1,6 @@
+# Object Detection (Computer Vision) > trained_v1
+https://universe.roboflow.com/texas-am-university-free/object-detection-computer-vision-2cuh3
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
